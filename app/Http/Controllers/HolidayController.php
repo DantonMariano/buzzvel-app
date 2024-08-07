b9928e56857315ccdedade8c29f3b6debf003289
DantonMariano <dantonrmariano@gmail.com>
2024-08-07 17:46:25 -0300

    creates CRUD operation for Holidays

---
+++ app/Http/Controllers/HolidayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Holiday;
+use Illuminate\Http\Request;
+
+class HolidayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Holiday::get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            "title" => ["required", "string"],
+            "description" => ["required", "string"],
+            "date" => ["required", "date"],
+            "participants" => "integer"
+        ]);
+
+        return Holiday::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $holiday = Holiday::find($id);
+        if (!$holiday) {
+            return response()->json(['message' => 'Holiday not found.'], 404);
+        }
+
+        return response()->json($holiday);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            "title" => ["required", "string"],
+            "description" => ["required", "string"],
+            "date" => ["required", "date"],
+            "participants" => "integer"
+        ]);
+
+        $holiday = Holiday::find($id);
+
+        if (!$holiday) {
+            return response()->json(['message' => 'Holiday not found.'], 404);
+        }
+
+        $holiday->update($data);
+
+        return response()->json($holiday);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $holiday = Holiday::find($id);
+
+        if (!$holiday) {
+            return response()->json(['message' => 'Holiday not found.'], 404);
+        }
+
+        $holiday->delete();
+
+        return response()->json(['message' => 'Holiday deleted successfully.']);
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\HolidayController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware("auth:sanctum")->group(function(){
+    Route::resource('holidays', HolidayController::class);
+    Route::get('download', [HolidayController::class, 'downloadPdf']);
+});
